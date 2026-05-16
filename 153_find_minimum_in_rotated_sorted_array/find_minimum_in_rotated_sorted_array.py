f5ec2c08bf2c3103146c9d37ede9a56d90024c5e
pawelbalcerek <pawbal96@gmail.com>
2026-05-16 18:23:40 +0200

    add 153. Find Minimum in Rotated Sorted Array solution
    with unit tests generated via gemini-cli (by Gemini 3 Flash)

---
+++ 153_find_minimum_in_rotated_sorted_array/find_minimum_in_rotated_sorted_array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def find_minimum_in_rotated_sorted_array(self, nums: list[int]) -> int:
+        l, r = 0, len(nums) - 1
+        result = nums[0]
+        while l <= r:
+            m = (l + r) // 2
+            if nums[l] <= nums[m]:
+                result = min(result, nums[l])
+                l = m + 1
+            else:
+                result = min(result, nums[m])
+                r = m - 1
+        return result
---
+++ 153_find_minimum_in_rotated_sorted_array/test_find_minimum_in_rotated_sorted_array.py
@@ -0,0 +1,39 @@
+import unittest
+from find_minimum_in_rotated_sorted_array import Solution
+
+class TestFindMinimumInRotatedSortedArray(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
+
+    def test_example1(self):
+        self.assertEqual(self.solution.find_minimum_in_rotated_sorted_array([3, 4, 5, 1, 2]), 1)
+
+    def test_example2(self):
+        self.assertEqual(self.solution.find_minimum_in_rotated_sorted_array([4, 5, 6, 7, 0, 1, 2]), 0)
+
+    def test_example3(self):
+        self.assertEqual(self.solution.find_minimum_in_rotated_sorted_array([11, 13, 15, 17]), 11)
+
+    def test_single_element(self):
+        self.assertEqual(self.solution.find_minimum_in_rotated_sorted_array([1]), 1)
+
+    def test_two_elements_sorted(self):
+        self.assertEqual(self.solution.find_minimum_in_rotated_sorted_array([1, 2]), 1)
+
+    def test_two_elements_rotated(self):
+        self.assertEqual(self.solution.find_minimum_in_rotated_sorted_array([2, 1]), 1)
+
+    def test_not_rotated(self):
+        self.assertEqual(self.solution.find_minimum_in_rotated_sorted_array([1, 2, 3, 4, 5]), 1)
+
+    def test_rotated_once(self):
+        self.assertEqual(self.solution.find_minimum_in_rotated_sorted_array([5, 1, 2, 3, 4]), 1)
+
+    def test_large_array(self):
+        nums = list(range(-5000, 5001))
+        # Rotate by 3000
+        rotated_nums = nums[3000:] + nums[:3000]
+        self.assertEqual(self.solution.find_minimum_in_rotated_sorted_array(rotated_nums), -5000)
+
+if __name__ == "__main__":
+    unittest.main()
